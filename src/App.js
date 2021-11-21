685d4a898b4a3cf413378040ea4e1c7a758afcc6
Wklej <kontogje@gmail.com>
2021-11-21 22:38:19 +0100

    + Material UI

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import Button from '@mui/material/Button';
 
 function App() {
   return (
-    <div>hello world</div>
+    <Button variant="contained" color="primary">
+      Hello button
+    </Button>
   );
 }
 
