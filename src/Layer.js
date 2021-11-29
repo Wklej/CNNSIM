d6c9d2193d47fb773464f170e330f0ba19b9e459
Wklej <kontogje@gmail.com>
2021-11-29 19:40:02 +0100

    handle disable button with switches

--- src/Layer.js
+++ src/Layer.js
@@ -11,6 +11,14 @@ const Layer = ({num}) => {
     const handlePoolShow = () => setPoolShow(true);
     const handlePoolClose = () => setPoolShow(false);
 
+    const handleDisableConv = (e) => {
+        const btnHandler = document.getElementById(e.target.id)
+        btnHandler.disabled = !btnHandler.disabled
+    }
+    const handleDisablePool = (e) => {
+        const btnHandler = document.getElementById(e.target.id)
+        btnHandler.disabled = !btnHandler.disabled
+    }
     const divs = () => {
         let array = [];
         for (let index = 0; index < num; index++) {
@@ -19,9 +27,15 @@ const Layer = ({num}) => {
                     <div className="card border-dark text-center" >
                         <div className="card-body">
                             <ul className="list-group">
-                                <button className="btn-primary" onClick={handleConvShow}>conv</button>
+                                <button className="btn-primary" onClick={handleConvShow} id={'conv' + index}>conv</button>
+                                <div className="form-check form-switch">
+                                    <input type="checkbox" className="form-check-input" id={'conv' + index} onChange={(e) => handleDisableConv(e)} />
+                                </div>
                                 <LayerModal show={showConv} handleClose={handleConvClose} body={"conv"} />
-                                <button className="btn-primary" onClick={handlePoolShow}>pool</button>
+                                <button className="btn-primary" onClick={handlePoolShow} id={'pool' + index}>pool</button>
+                                <div className="form-check form-switch">
+                                    <input type="checkbox" className="form-check-input" id={'pool' + index} onChange={(e) => handleDisablePool(e)} />
+                                </div>
                                 <LayerModal show={showPool} handleClose={handlePoolClose} body={'pool'} />
                             </ul>
                         </div>
