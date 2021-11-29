f8658ed64fd433834d504261206b5b01888a1bd7
Wklej <kontogje@gmail.com>
2021-11-29 14:26:22 +0100

    body type moved to modal itself

--- src/Layer.js
+++ src/Layer.js
@@ -1,6 +1,5 @@
 import { useState } from "react";
 import LayerModal from "./LayerModal";
-import { Modal } from "react-bootstrap";
 
 const Layer = ({num}) => {
 
@@ -12,14 +11,6 @@ const Layer = ({num}) => {
     const handlePoolShow = () => setPoolShow(true);
     const handlePoolClose = () => setPoolShow(false);
 
-    const [convBody, setConvBody] = useState(
-        <Modal.Body>conv body</Modal.Body>
-    );
-
-    const [poolBody, setPoolBody] = useState(
-        <Modal.Body>pool body</Modal.Body>
-    );
-
     const divs = () => {
         let array = [];
         for (let index = 0; index < num; index++) {
@@ -29,9 +20,9 @@ const Layer = ({num}) => {
                         <div className="card-body">
                             <ul className="list-group">
                                 <button className="btn-primary" onClick={handleConvShow}>conv</button>
-                                <LayerModal show={showConv} handleClose={handleConvClose} body={convBody} />
+                                <LayerModal show={showConv} handleClose={handleConvClose} body={"conv"} />
                                 <button className="btn-primary" onClick={handlePoolShow}>pool</button>
-                                <LayerModal show={showPool} handleClose={handlePoolClose} body={poolBody} />
+                                <LayerModal show={showPool} handleClose={handlePoolClose} body={'pool'} />
                             </ul>
                         </div>
                         <div className="card-footer">params</div>
--- src/LayerModal.js
+++ src/LayerModal.js
@@ -2,19 +2,34 @@ import { Modal } from "react-bootstrap";
 
 const LayerModal = ({show, handleClose, body}) => {
 
+    const bodyType = () => {
+        if (body === "conv") {
+            console.log('conv')
+            return(
+                <Modal.Body>conv body</Modal.Body>
+            )
+        }
+        else if(body === "pool") {
+            console.log('pool')
+            return(
+                <Modal.Body>pool body</Modal.Body>
+            )
+        }
+    }
+
     return ( 
         <Modal show={show} onHide={handleClose}>
             <Modal.Header closeButton>
             <Modal.Title>Modal heading</Modal.Title>
             </Modal.Header>
-            { body }
+            { bodyType() }
             <Modal.Footer>
-            <button className="btn-secondary" onClick={handleClose}>
-                Close
-            </button>
-            <button className="btn-primary" onClick={handleClose}>
-                Save Changes
-            </button>
+                <button className="btn-secondary" onClick={handleClose}>
+                    Close
+                </button>
+                <button className="btn-primary" onClick={handleClose}>
+                    Save Changes
+                </button>
             </Modal.Footer>
         </Modal>
      );
