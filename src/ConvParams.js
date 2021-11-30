5a972356bbf33c358e60512152d83f5b192cf500
Wklej <kontogje@gmail.com>
2021-11-30 18:59:31 +0100

    + Features modal

---
+++ src/ConvParams.js
@@ -0,0 +1,9 @@
+import { Modal } from "react-bootstrap";
+
+const ConvParams = () => {
+    return ( 
+        <Modal.Body>Conv params</Modal.Body>
+     );
+}
+ 
+export default ConvParams;
--- src/Layer.js
+++ src/Layer.js
@@ -1,15 +1,19 @@
 import { useState } from "react";
 import LayerModal from "./LayerModal";
+import Features from "./Features";
 
 const Layer = ({num}) => {
 
     const [showConv, setConvShow] = useState(false);
     const [showPool, setPoolShow] = useState(false);
+    const [showFeatures, setShowFeatures] = useState(false);
 
     const handleConvShow = () => setConvShow(true);
     const handleConvClose = () => setConvShow(false);
     const handlePoolShow = () => setPoolShow(true);
     const handlePoolClose = () => setPoolShow(false);
+    const handleFeaturesShow = () => setShowFeatures(true);
+    const handleFeaturesClose = () => setShowFeatures(false);
 
     const handleDisableConv = (e) => {
         const btnHandler = document.getElementById(e.target.id)
@@ -39,7 +43,10 @@ const Layer = ({num}) => {
                                 <LayerModal show={showPool} handleClose={handlePoolClose} body={'pool'} />
                             </ul>
                         </div>
-                        <div className="card-footer">params</div>
+                        <div className="card-footer">
+                            <button className="btn-primary" onClick={handleFeaturesShow} id={'feat' + index}>Features</button>
+                            <Features show={showFeatures} handleClose={handleFeaturesClose} />
+                        </div>
                     </div>
                 </div>
             )
