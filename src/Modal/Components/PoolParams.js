b78f4a752f64db1cd1cd69c0040b27272df5cced
Wklej <kontogje@gmail.com>
2021-12-01 20:18:37 +0100

    Details tab - params to set (basic)

--- src/Modal/Components/PoolParams.js
+++ src/Modal/Components/PoolParams.js
@@ -2,7 +2,25 @@ import { Modal } from "react-bootstrap";
 
 const PoolParams = () => {
     return ( 
-        <Modal.Body>Pool params</Modal.Body>
+        <Modal.Body>
+            Pool params
+            <div className="input-group">
+                <span className="input-group-text">Pool size:</span>
+                <select className="form-select" aria-label="Default select example">
+                    <option selected>Open this select menu</option>
+                    <option value="1">One</option>
+                    <option value="2">Two</option>
+                    <option value="3">Three</option>
+                </select>
+                <span className="input-group-text">Stride:</span>
+                <select className="form-select" aria-label="Default select example">
+                    <option selected>Open this select menu</option>
+                    <option value="1">One</option>
+                    <option value="2">Two</option>
+                    <option value="3">Three</option>
+                </select>
+            </div>
+        </Modal.Body>
      );
 }
  
