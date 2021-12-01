29a677529cf157327a0af96ad3b24a74aaedb6a3
Wklej <kontogje@gmail.com>
2021-12-01 20:33:40 +0100

    + - buttons for adding nodes

--- src/Layers/Fully.js
+++ src/Layers/Fully.js
@@ -1,14 +1,34 @@
+import { useState } from "react";
+
 const Fully = () => {
+
+    const [num, setNum] = useState(1)
+
+    const handlePlus = () => setNum(num+1)
+    const handleMinus = () => setNum(num-1)
+
+    const nodes = () => {
+        let array = [];
+        for (let index = 0; index < num; index++) {
+            array.push(
+                <button className="btn btn-primary mb-2" id={index}>{"Node " + index}</button>
+            )
+        }
+        return array
+    }
+
+
     return ( 
         <div className="col">
             <div className="card border-dark text-center" >
                 <div className="card-body">
                     <ul className="list-group">
-                        <button className="btn btn-primary">node 1</button>
+                        { nodes() }
                     </ul>
                 </div>
                 <div className="card-footer">
-                    Params ? plus minus ?
+                    <button className="btn btn-outline-primary me-2" onClick={handlePlus}>+</button>
+                    <button className="btn btn-outline-primary" onClick={handleMinus}>-</button>
                 </div>
             </div>
         </div>
