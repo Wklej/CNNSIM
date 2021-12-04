b467f0060ccda8ffd8bad012688452c16ab55d46
Wklej <kontogje@gmail.com>
2021-12-04 13:41:14 +0100

    + Context value passing to grandchild components

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import { useState } from "react";
 import TopBar from './TopBar';
 import Workflow from './Workflow';
 
+import { testContext } from './testContext';
+
 function App() {
 
     // Utils for tracking slider value
@@ -12,11 +14,93 @@ function App() {
         setNumLayers(e.valueOf())
     }
 
+    //global values to pass to context and use in grandchild components
+    const [convValues, setConvValues] = useState({
+        filters: '1', kernel: '1', activation: '1', stride: '1'
+    })
+
+    const handleConvChange = (e) => {
+        setConvValues( vals => {
+            return { ...vals, [e.target.name]: e.target.value}
+        })
+    }
+
+
+    var persons = [
+        {name: ["Filip", "Filip"], age: "22"},
+        {name: ["Natalia", "Natalia"], age: "24"},
+        {name: ["Fufu", "Fufu"], age: "42"},
+    ]
+
+    var persons2 = [
+        {name: ["Filip", "Filip"], age: "22"},
+        {name: ["Natalia", "Nataliaa"], age: "24"},
+        {name: ["Fufu", "Fufu"], age: "42"},
+    ]
+
+    var test = {
+        layers: [
+            {conv: [{filters: 32}, {stride: '2x2'}], pool: 2, drop: 3},
+            {conv: [{filters: 64}, {stride: '4x4'}], pool: 2, drop: 3},
+        ],
+        fully: [
+            {x: 1, y: 3, z: 5},
+            {x: 1, y: 3, z: 5},
+        ] 
+    }
+
+    var test2 = {
+        layers: [
+            {conv: [{filters: 32}, {stride: '2x2'}], pool: 2, drop: 3},
+            {conv: [{filters: 14}, {stride: '4x4'}], pool: 2, drop: 3},
+        ],
+        fully: [
+            {x: 1, y: 3, z: 5},
+            {x: 1, y: 3, z: 5},
+        ] 
+    }
+
+    const loop = () => {
+        const arr = []
+        for (let index = 0; index < 3; index++) {
+            if (JSON.stringify(persons[index]) === JSON.stringify(persons2[index])) {
+                console.log(persons[index], persons2[index])
+                arr.push(1)
+            }
+            else arr.push(0)
+        }
+        console.log(test)
+        return arr;
+    }
+
+    const testt = () => {
+        const array = []
+        if (JSON.stringify(test) === JSON.stringify(test2)) {
+            array.push(1)
+        } else array.push(0 )
+        
+            return array;
+    }
+
+
     return (
         <div>
-            <TopBar numLayers={numLayers} handleSliderChange={handleSliderChange} />      
-            <Workflow numLayers={numLayers} />
+            <testContext.Provider value={handleConvChange}>
+                <TopBar numLayers={numLayers} handleSliderChange={handleSliderChange} />      
+                <Workflow numLayers={numLayers} />
+                {console.log(convValues)}
+            </testContext.Provider>
+            <hr />
+
+            {
+                
+                // loop()
+                testt()
+                
+            }
+
         </div>
+
     );
 }
 
--- src/Modal/Components/ConvParams.js
+++ src/Modal/Components/ConvParams.js
@@ -1,42 +1,41 @@
+import { useContext } from "react";
 import { Modal } from "react-bootstrap";
+import { testContext } from "../../testContext";
 
 const ConvParams = () => {
+
+    const handleChange = useContext(testContext)
+
     return ( 
         <Modal.Body>
             <div className="input-group">
                 <span className="input-group-text">Filters:</span>
-                <select className="form-select" aria-label="Default select example">
-                    <option selected>Open this select menu</option>
-                    <option value="1">One</option>
-                    <option value="2">Two</option>
-                    <option value="3">Three</option>
+                <select className="form-select" name="filters" aria-label="Default select example" onChange={handleChange}>
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
                 </select>
                 <span className="input-group-text">Kernel size:</span>
-                <select className="form-select" aria-label="Default select example">
-                    <option selected>Open this select menu</option>
-                    <option value="1">One</option>
-                    <option value="2">Two</option>
-                    <option value="3">Three</option>
+                <select className="form-select" name="kernel" aria-label="Default select example" onChange={handleChange}>
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
                 </select>
             </div>
             <div className="input-group">
                 <span className="input-group-text">Activation:</span>
-                <select className="form-select" aria-label="Default select example">
-                    <option selected>Open this select menu</option>
-                    <option value="1">One</option>
-                    <option value="2">Two</option>
-                    <option value="3">Three</option>
+                <select className="form-select" name="activation" aria-label="Default select example" onChange={handleChange}>
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
                 </select>
                 <span className="input-group-text">Stride:</span>
-                <select className="form-select" aria-label="Default select example">
-                    <option selected>Open this select menu</option>
-                    <option value="1">One</option>
-                    <option value="2">Two</option>
-                    <option value="3">Three</option>
+                <select className="form-select" name="stride" aria-label="Default select example" onChange={handleChange}>
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
                 </select>
-            </div>
-        
-        
+            </div>     
         </Modal.Body>
      );
 }
