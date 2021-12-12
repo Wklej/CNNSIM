f7cecb4cf4c1027cb571088604964af6d3524acc
Wklej <kontogje@gmail.com>
2021-12-12 18:03:42 +0100

    passing value based on label

--- src/Modal/Components/SelectBundle.js
+++ src/Modal/Components/SelectBundle.js
@@ -11,7 +11,7 @@ const SelectBundle = ({values, label, update, id}) => {
         <div className="input-group input-group-sm">
             <button className="btn btn-outline-secondary btn-sm" onClick={handleModalShow}>{label}:</button>  
             <ExplainModal show={showModal} handleClose={handleModalClose} type={label} />
-            <select className="form-select" name={label} value={values.loss} onChange={(e) => update(e, id)}>
+            <select className="form-select" name={label} value={values[label]} onChange={(e) => update(e, id)}>
                 <option>1</option>
                 <option>2</option>
                 <option>3</option>
