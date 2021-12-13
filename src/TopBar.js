fdf852ce106b1af68597e05c87c9f7a7dd59c3c2
Wklej <kontogje@gmail.com>
2021-12-13 15:04:02 +0100

    + increasing/decreasing epoch status

--- src/TopBar.js
+++ src/TopBar.js
@@ -1,7 +1,26 @@
 import Slider from 'rc-slider';
 import 'rc-slider/assets/index.css';
+import { useState } from 'react';
 
 const TopBar = ({numLayers, handleSliderChange}) => {
+
+    const epochs = [1, 5, 10, 15, 30]
+
+    const [epoch, setEpoch] = useState(0)
+
+    const handlePlus = () => {
+        const x = epoch + 1
+        if (x < epochs.length) {
+            setEpoch(x)
+        }
+    }
+
+    const handleMinus = () => {
+        const x = epoch - 1
+        if (x >= 0) {
+            setEpoch(x)
+        }
+    }
 
     return ( 
         <div className="container">
@@ -10,7 +29,11 @@ const TopBar = ({numLayers, handleSliderChange}) => {
                     <Slider min={2} max={6} dots={true} marks={{2:2, 3:3, 4:4, 5:5, 6:6}}
                             value={numLayers} onChange={handleSliderChange} />
                 </div>
-                <div className="col-4 p-2 bg-primary text-ligh">epochs status</div>
+                <div className="col-4 p-2 bg-primary text-ligh">
+                    Current epoch: {epochs[epoch]}
+                    <button className="btn btn-success mx-2" onClick={handlePlus}>+</button>
+                    <button className="btn btn-success mx-2" onClick={handleMinus}>-</button>
+                </div>
             </div>
       </div>
      );
