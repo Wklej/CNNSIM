6b4d1b8f5879e782e85c8bdb2694172a0c99cba2
Wklej <kontogje@gmail.com>
2021-12-13 13:58:40 +0100

    + passing images to modal based on ModelID

--- src/Modal/Components/ModelSet.js
+++ src/Modal/Components/ModelSet.js
@@ -1,6 +1,13 @@
 import { useState } from "react";
 import ImageModal from "../ImageModal";
 
+import dog1 from '../../Images/dog1.jpg'
+import dog2 from '../../Images/dogo2.jpg'
+import dog3 from '../../Images/dogo4.jpg'
+import letter1 from '../../Images/1.png'
+import letter2 from '../../Images/2.png'
+import letter3 from '../../Images/3.png'
+
 const ModelSet = ({modelID, handleModelChange, handleImageChange}) => {
 
     const [show, setShow] = useState(false);
@@ -8,12 +15,21 @@ const ModelSet = ({modelID, handleModelChange, handleImageChange}) => {
     const handleShow = () => setShow(true);
     const handleClose = () => setShow(false);
 
+    const [imgs] = useState(
+            {
+                1 : [dog1, dog2, dog3],
+                2 : [letter1, letter2, letter3],
+                3 : []
+            }
+        )
+
     return ( 
         <div className="btn-group" role="group">
             <input className="btn-check" type="radio" name="flexRadio" id={modelID} onClick={(e) => handleModelChange(e)} />
             <label className="btn btn-outline-dark mb-2" htmlFor={modelID}>Set {modelID}</label>
             <button className="btn btn-outline-dark mb-2" onClick={handleShow}>img</button>
-            <ImageModal show={show} handleClose={handleClose} handleImageChange={handleImageChange} />
+            <ImageModal show={show} handleClose={handleClose}
+                        handleImageChange={handleImageChange} images={imgs[modelID]} />
         </div>
      );
 }
--- src/Modal/ImageModal.js
+++ src/Modal/ImageModal.js
@@ -1,12 +1,7 @@
 import { Modal } from "react-bootstrap";
 import ImageSelection from "./Components/ImageSelection";
 
-import dog1 from '../Images/dog1.jpg'
-import dog2 from '../Images/dogo2.jpg'
-import dog3 from '../Images/dogo4.jpg'
-
-
-const ImageModal = ({show, handleClose, handleImageChange}) => {
+const ImageModal = ({show, handleClose, handleImageChange, images}) => {
     return ( 
         <>
             <Modal show={show} onHide={handleClose}>
@@ -14,11 +9,11 @@ const ImageModal = ({show, handleClose, handleImageChange}) => {
                 <Modal.Body>
                     <div className="d-flex justify-content-evenly">
                         <ImageSelection handleChange={handleImageChange} handleClose={handleClose}
-                                        imageID={4} imgPath={dog1} />
+                                        imageID={4} imgPath={images[0]} />
                         <ImageSelection handleChange={handleImageChange} handleClose={handleClose}
-                                        imageID={5} imgPath={dog2} />
+                                        imageID={5} imgPath={images[1]} />
                         <ImageSelection handleChange={handleImageChange} handleClose={handleClose}
-                                        imageID={6} imgPath={dog3} />
+                                        imageID={6} imgPath={images[2]} />
                     </div>
                 </Modal.Body>
                 <Modal.Footer>
