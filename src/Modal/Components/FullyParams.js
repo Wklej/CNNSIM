95ae75cadcf9a5b0898f6a1db7df7e9368997212
Wklej <kontogje@gmail.com>
2021-12-14 18:46:52 +0100

    params replaced with SelectBundle component

--- src/Modal/Components/FullyParams.js
+++ src/Modal/Components/FullyParams.js
@@ -2,6 +2,7 @@ import { useContext, useState } from "react";
 import { Modal } from "react-bootstrap";
 import { paramContext } from "../../paramContext";
 import { testContext } from "../../testContext";
+import SelectBundle from "./SelectBundle";
 
 const FullyParams = ({id, layerType}) => {
 
@@ -17,20 +18,8 @@ const FullyParams = ({id, layerType}) => {
 
     return ( 
         <Modal.Body>
-            <div className="input-group">
-                <span className="input-group-text">Filters:</span>
-                <select className="form-select" name="filters" value={values.filters} aria-label="Default select example" onChange={(e) => update(e, id)}>
-                    <option>1</option>
-                    <option>2</option>
-                    <option>3</option>
-                </select>
-                <span className="input-group-text">Activation:</span>
-                <select className="form-select" name="activation" value={values.activation} aria-label="Default select example" onChange={(e) => update(e, id)}>
-                    <option>1</option>
-                    <option>2</option>
-                    <option>3</option>
-                </select>
-            </div>
+            <SelectBundle values={values} label='filters' update={update} id={id} optionValues={['1', '2', '3']} />
+            <SelectBundle values={values} label='activation' update={update} id={id} optionValues={['1', '2', '3']} />
         </Modal.Body>
      );
 }
