96b1c3d20e0d1d587222b6ad719fdc0931585f56
Wklej <kontogje@gmail.com>
2021-12-28 19:45:15 +0100

    +WIP: Loading images based on epoch status

--- src/Modal/Components/Features.js
+++ src/Modal/Components/Features.js
@@ -2,14 +2,20 @@ import Tabs from 'react-bootstrap/Tabs'
 import { Modal, Tab } from "react-bootstrap";
 import { useState } from 'react';
 import Karuzela from './Karuzela';
+import { epochsContext } from '../../epochsContext';
+import { useContext } from 'react';
 
 const Features = ({show, handleClose, idx}) => {
+
+    //Current epoch status
+    const epoch = useContext(epochsContext)
 
     // Function to map whole dir content to @images object
     const getImages = (dirVariable) => {
         const cache = {};
 
         const r = dirVariable
+
         r.keys().forEach((key) => (cache[key] = r(key)))
     
         const images = Object.entries(cache).map(module => module[1].default);
@@ -21,25 +27,59 @@ const Features = ({show, handleClose, idx}) => {
     const getDir = (layerType) => {
         switch (layerType) {
             case "activations":
-                return require.context("../../Images/Activations/test_dir", false, /\.(png|gif|jpg)$/)
+                switch (epoch) {
+                    case 1:
+                        return require.context("../../Images/Activations/test/1/outputs", false, /\.(png|gif|jpg)$/)        
+                        break;
+                    case 5:
+                        return require.context("../../Images/Activations/test/5/outputs", false, /\.(png|gif|jpg)$/)        
+                        break;
+                    case 15:
+                        return require.context("../../Images/Activations/test/15/outputs", false, /\.(png|gif|jpg)$/)        
+                        break;
+                    case 20:
+                        return require.context("../../Images/Activations/test/20/outputs", false, /\.(png|gif|jpg)$/)        
+                        break;
+                    case 30:
+                        return require.context("../../Images/Activations/test/30/outputs", false, /\.(png|gif|jpg)$/)
+                        break;
+                    default:
+                        break;
+                }
+            case "filters":
+                switch (epoch) {
+                    case 1:
+                        return require.context("../../Images/Activations/test/1/filters", false, /\.(png|gif|jpg)$/)        
+                        break;
+                    case 5:
+                        return require.context("../../Images/Activations/test/5/filters", false, /\.(png|gif|jpg)$/)        
+                        break;
+                    case 15:
+                        return require.context("../../Images/Activations/test/15/filters", false, /\.(png|gif|jpg)$/)        
+                        break;
+                    case 20:
+                        return require.context("../../Images/Activations/test/20/filters", false, /\.(png|gif|jpg)$/)        
+                        break;
+                    case 30:
+                        return require.context("../../Images/Activations/test/30/filters", false, /\.(png|gif|jpg)$/)
+                        break;
+                    default:
+                        break;
+                }
             default:
                 break;
         }
     }
 
-
     const [type, setType] = useState('activations')
 
     const bodyType = () => {
-        if (type === "activations") {
+        if (type === "activations" || type === "filters") {
             return(
                 <Modal.Body>
                     <Karuzela content={getImages(getDir(type))} idx={idx} />
                 </Modal.Body>
             )    
-        }
-        else if(type === "filters") {
-            return(<Modal.Body>Feature 2</Modal.Body>)
         }
         else if(type === "comparison") {
             return(<Modal.Body>Feature 3</Modal.Body>)
@@ -56,7 +96,7 @@ const Features = ({show, handleClose, idx}) => {
                     { bodyType() }
                 </Tab>   
                 <Tab eventKey="comparison" title="Comparison">
-                    { bodyType() }
+                    { epoch }
                 </Tab>   
             </Tabs>
             <Modal.Footer>
