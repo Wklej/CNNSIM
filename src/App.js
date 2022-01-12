d6c1d7c23e0aef18e31a0898a2b0cedaa540f1d5
Wklej <kontogje@gmail.com>
2022-01-12 23:27:01 +0100

    init dropout change

--- src/App.js
+++ src/App.js
@@ -52,11 +52,11 @@ function App() {
         layers: [
             {   conv: {filters: '16', kernel_size: '(2, 2)', activation: 'relu'},
                 pool: {pool_size: '(2, 2)', stride: '2'},
-                drop: 10
+                drop: 20
             },
             {   conv: {filters: '32', kernel_size: '(2, 2)', activation: 'relu'},
                 pool: {pool_size: '(2, 2)', stride: '2'},
-                drop: 10
+                drop: 20
             },
             {   conv: {filters: null, kernel_size: null, activation: null},
                 pool: {pool_size: null, stride: null},
