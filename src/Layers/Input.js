d349e9a7345304a640cb60eea287d4870d9ffcb2
Wklej <kontogje@gmail.com>
2022-01-16 20:05:07 +0100

    params on bottom

--- src/Layers/Input.js
+++ src/Layers/Input.js
@@ -9,27 +9,28 @@ const Input = ({handleSliderChange, handleModelChange, handleImageChange, setLos
     const [toggleImageButton, setToggleImageButton] = useState(1)
 
     return ( 
-        <div className="col text-center">
+        <div className="col text-center position-relative">
             <div>
                 <ModelSet modelID={1} handleModelChange={handleModelChange} handleImageChange={handleImageChange}
                         check={true} setLossFunc={setLossFunc} toggle={toggleImageButton} setToggle={setToggleImageButton} />
                 <ModelSet modelID={2} handleModelChange={handleModelChange} handleImageChange={handleImageChange}
                         setLossFunc={setLossFunc} toggle={toggleImageButton} setToggle={setToggleImageButton} />
             </div>
-            <div>
-                Mid Layers count:
-                <Slider min={2} max={4} dots={true} marks={{2:2, 3:3, 4:4}}
-                        value={numLayers} onChange={handleSliderChange} />
+            <div className="position-absolute bottom-0">
+                <div>
+                    Mid Layers count:
+                    <Slider min={2} max={4} dots={true} marks={{2:2, 3:3, 4:4}}
+                            value={numLayers} onChange={handleSliderChange} />
+                </div>
+                <div className="my-4">
+                    Current epoch: {status}
+                        <button className="btn btn-success mx-2" onClick={handlePlus}>+</button>
+                        <button className="btn btn-success mx-2" onClick={handleMinus}>-</button>
+                </div>
+                <div>
+                    <button className='btn btn-primary' onClick={compare}>Simulation</button>
+                </div>
             </div>
-            <div className="my-4">
-                Current epoch: {status}
-                    <button className="btn btn-success mx-2" onClick={handlePlus}>+</button>
-                    <button className="btn btn-success mx-2" onClick={handleMinus}>-</button>
-            </div>
-            <div>
-                <button className='btn btn-primary' onClick={compare}>Simulation</button>
-            </div>
-        
         
         </div>
     );
