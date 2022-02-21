a4aa119f5408404fd87f391e02a141a100cb4fbb
Wklej <kontogje@gmail.com>
2022-02-21 20:47:30 +0100

    removing switches

--- src/Modal/Components/Dropout.js
+++ src/Modal/Components/Dropout.js
@@ -4,7 +4,6 @@ import Slider from 'rc-slider';
 import 'rc-slider/assets/index.css';
 import ExplainModal from "../ExplainModal";
 import { paramContext } from "../../paramContext";
-import { testContext } from "../../testContext";
 
 const Dropout = ({id}) => {
 
@@ -13,30 +12,13 @@ const Dropout = ({id}) => {
     const [showModal, setModalShow] = useState(false);
     const handleModalShow = () => setModalShow(true);
     const handleModalClose = () => setModalShow(false);
-    
-    const handleChange = useContext(testContext)
+
     const getValues = useContext(paramContext)
     
     const [values, setValues] = useState(getValues(id, 'drop'))
-    
-    const update = (e, id) => {
-        setValues(e.valueOf() * 20)
-        handleChange(e, id, 'drop')
-    }
+
 
     const [button_disabled, setButton_disabled] = useState(true)
-
-    const handleDisableDrop = () => {
-        setOpen(false)
-        setButton_disabled(!button_disabled)
-
-        if (!button_disabled)
-            handleChange(null, id, 'dropDefault')
-        else {
-            handleChange(20, id, 'dropDefault')
-            setValues(20)
-        }
-    }
     
     const orangeBg = {backgroundColor: "#e38627"}
     const dotStyle = {backgroundColor: "#212121", border: "solid 2px #212121"}
@@ -45,25 +27,22 @@ const Dropout = ({id}) => {
     return ( 
         <div>
         <div className="btn-group" role="group">
-            <button className="btn btn-orange" name={'drop' + id} disabled={button_disabled} onClick={handleModalShow}>Dropout</button>
+            <button className="btn btn-orange" name={'drop' + id} disabled={!button_disabled} onClick={handleModalShow}>Dropout</button>
             <ExplainModal show={showModal} handleClose={handleModalClose} type={'dropout'} />
-            <button className="btn btn-orange" aria-controls="collapseImages" disabled={button_disabled}
+            <button className="btn btn-orange" aria-controls="collapseImages" disabled={!button_disabled}
                     aria-expanded={open} name={'drop' + id} id={'drop' + id} onClick={() => setOpen(!open)}>
                 x
             </button>
         </div>
             <Collapse in={open}>
                 <div id="collapseImages">
+                    <p></p>
                     <Slider railStyle={orangeBg} trackStyle={orangeBg} dotStyle={dotStyle} handleStyle={handleStyle}
-                            min={1} max={2} dots={true} marks={{1:20, 2:40}}
-                            value={values / 20} onChange={(e) => update(e, id)} />
+                            min={1} max={2} dots={true} marks={{1:0, 2:30}} value={values / 20} />
                     {/* Empty paragraph to avoid numbers to overlap switch */}
                     <p></p>
                 </div>
             </Collapse>
-            <div className="form-check form-switch">
-                <input type="checkbox" className="form-check-input" id={'drop' + id} onChange={() => handleDisableDrop()} />
-            </div>
         </div>
      );
 }
